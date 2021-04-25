ce275dab266ec2afbc67e0275d38083e1910d331
valerij <job.valerij.gopkalo@gmail.com>
2021-04-25 11:47:35 +0300

    add ticket template

--- src/desktop/modules/search/view/Search.js
+++ src/desktop/modules/search/view/Search.js
@@ -1,10 +1,14 @@
 import React from "react";
 
+import Ticket from "components/ticket/Ticket";
+
 class Search extends React.Component {
     render() {
         return (
             <section className="search">
                 Hello SEARCH PAGE
+
+                <Ticket />
 
                 <div className="outlined-text-form">
                     <input type="text" className="form-control" required />
