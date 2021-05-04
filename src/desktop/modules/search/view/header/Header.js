309354720c56909f244d9f8140461d8c96a46bc2
roman.bogdanov <cflatminor92@gmail.com>
2021-05-04 15:54:38 +0300

    fix & refact

--- src/desktop/modules/search/view/header/Header.js
+++ src/desktop/modules/search/view/header/Header.js
@@ -1,10 +1,6 @@
 import React from "react";
 
-import Destination from "./Destination";
-import DeparturePoint from "./DeparturePoint";
-import ArrivalDate from "./ArrivalDate";
-import DepartureDate from "./DepartureDate";
-import Passengers from "./Passengers";
+import Route from "../route/Route";
 
 class Header extends React.Component {
     /**
@@ -15,50 +11,21 @@ class Header extends React.Component {
     render() {
         return (
             <header className="search__header">
-                <div className="search__title">
-                    <div className="container-fluid">
-                        <div className="row">
-                            <div className="col">
-                                <h1 className="text-center">
-                                    Поиск и бронирование билетов
-                                </h1>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col">
+                            <h1 className="search__title text-center">
+                                Поиск и бронирование билетов
+                            </h1>
 
-                                <p className="text-center">
-                                    Лучший способ купить авиабилеты дёшево
-                                </p>
-                            </div>
+                            <p className="search__subtitle text-center">
+                                Лучший способ купить авиабилеты дёшево
+                            </p>
                         </div>
                     </div>
                 </div>
 
-                <div className="search__route route">
-                    <div className="container-fluid">
-                        <div className="row">
-                            <div className="col">
-                                <div className="route__form d-flex">
-                                    <div className="route__inputs d-flex w-100">
-                                        <DeparturePoint />
-
-                                        <Destination />
-
-                                        <ArrivalDate />
-
-                                        <DepartureDate />
-
-                                        <Passengers />
-                                    </div>
-
-                                    <button
-                                        className="btn-default btn-lg to-search-results"
-                                        type="button"
-                                    >
-                                        Найти
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <Route />
             </header>
         );
     }
