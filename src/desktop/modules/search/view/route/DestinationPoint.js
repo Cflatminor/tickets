9b21234ef1126d48a503821475aa3ca958f2fdf8
roman.bogdanov <cflatminor92@gmail.com>
2021-05-31 20:08:43 +0300

    wip header

--- src/desktop/modules/search/view/route/DestinationPoint.js
+++ src/desktop/modules/search/view/route/DestinationPoint.js
@@ -8,25 +8,92 @@ class DestinationPoint extends React.Component {
         super(props);
 
         this.state = {
+            selectedItem: {
+                getName: () => "",
+                getCode: () => ""
+            },
             isFocused: false
         };
 
         this._getItemsByQuery = this._getItemsByQuery.bind(this);
+        this._selectItem = this._selectItem.bind(this);
         this._onFocus = this._onFocus.bind(this);
         this._onBlur = this._onBlur.bind(this);
     }
 
     _getItemsByQuery(query, success) {
-        let items = ["qw", "qwe", "qwer", "qwert", "qwerty", "qwertyu"],
-            result = items
-                .filter((item) => item.includes(query))
-                .map((item) => ({getName: () => item}));
+        if (query.length < 2) {
+            this.setState({
+                selectedItem: {
+                    getName: () => "",
+                    getCode: () => ""
+                }
+            });
+        }
 
         if (query.length > 2) {
+            let items = [
+                    {
+                        country_code: "TR",
+                        code: "IST",
+                        name: "Стамбул",
+                        airports: [
+                            {
+                                full_name: "Стамбул Новый аэропорт, Стамбул",
+                                code: "IST",
+                                name: "Стамбул Новый аэропорт"
+                            },
+                            {
+                                full_name: "Сабиха Гёкчен, Стамбул",
+                                code: "SAW",
+                                name: "Сабиха Гёкчен"
+                            }
+                        ]
+                    },
+                    {
+                        country_code: "RU",
+                        code: "STW",
+                        name: "Ставрополь",
+                        airports: [
+                            {
+                                full_name: "Ставрополь, Ставрополь",
+                                code: "STW",
+                                name: "Ставрополь"
+                            }
+                        ]
+                    },
+                    {
+                        country_code: "NO",
+                        code: "SVG",
+                        name: "Ставангер",
+                        airports: [
+                            {
+                                full_name: "Сула, Ставангер",
+                                code: "SVG",
+                                name: "Сула"
+                            }
+                        ]
+                    }
+                ],
+                result = items
+                    .filter((item) => item.name.toLowerCase().includes(query.toLowerCase()))
+                    .map((item) => ({
+                        getName: () => item.name,
+                        getCode: () => item.code
+                    }));
+
             success(result);
         } else {
             success([]);
         }
+    }
+
+    _selectItem(item) {
+        this.setState({
+            selectedItem: item
+        });
+
+        return this;
     }
 
     _onFocus() {
@@ -54,6 +121,7 @@ class DestinationPoint extends React.Component {
                 <Autocomplete
                     placeholder=""
                     getItemsByQuery={this._getItemsByQuery}
+                    selectItem={this._selectItem}
                     onFocus={this._onFocus}
                     onBlur={this._onBlur}
                 />
@@ -61,6 +129,10 @@ class DestinationPoint extends React.Component {
                 <label>
                     Куда
                 </label>
+
+                <span className="city-code">
+                    { this.state.selectedItem.getCode() }
+                </span>
             </div>
         );
     }
